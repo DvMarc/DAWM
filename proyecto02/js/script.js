a6610816397ec5c12b2f0b76c4128faaa6bea359
DvMarc <85300106+DvMarc@users.noreply.github.com>
2022-11-23 08:58:14 -0600

    Add files via upload

--- proyecto02/js/script.js
+++ proyecto02/js/script.js
@@ -1,30 +1,45 @@
-const llamadaApi = async () =>{
-    await fetch("https://protected-taiga-89091.herokuapp.com/api/card")
-      .then(response => response.json() )  
-      .then( data => {
-        const body = document.querySelector('body');
-        const section = document.createElement('section');
-        const cartas = data["data"];
-        for (index in cartas){
-          const carta = cartas[index];
-          const kanji = carta["kanji"];
-          const nameES = carta["spanishName"];
-          const romaji = carta["Rōmaji"];
-          
-          if (kanji !== undefined) {
-            let plantilla = 
-            `<tr>
-              <td>${kanji}</td>
-              <td>${nameES}</td>
-              <td>${romaji}</td>
-            </tr>`
-            document.querySelector("table").innerHTML += plantilla;
-          }
-        }
-      }
-    )
+const apiMonsters = "https://www.dnd5eapi.co/api/monsters";
+const apiEquipment = "https://www.dnd5eapi.co/api/equipment";
+
+let opciones;
+
+const cargarOpciones = async () =>{
+    let select = document.getElementsByClassName('box')[0]
+    let optionMonsters = document.createElement('option')
+    let optionEquipment = document.createElement('option')
+    optionMonsters.innerHTML = "Monsters";
+    optionMonsters.setAttribute("value",apiMonsters)
+    optionEquipment.innerHTML = "Equipment";
+    optionEquipment.setAttribute("value",apiEquipment)
+    select.appendChild(optionMonsters)
+    select.appendChild(optionEquipment)
+}
+
+let select = document.getElementsByClassName('box')[0]
+select.addEventListener("change", async (event) => {
+    let valor = event.target.value;
+    await fetch(valor)
+    .then(response => response.json())
+    .then(data => {
+        opciones = data["results"]
+        //cargarDatos(opciones, valor)
+    })
+})
+
+let cargarDatos = async (opciones, valor) =>{
+    let raw = document.getElementsByClassName('raw')[0]
+    raw.innerHTML = ""
+    for (let i = 0; i <opciones.length; ++i){
+        let object = opciones[i]["index"]
+        let urlObject = valor+'/'+object
+        await fetch(urlObject)
+        .then(response => response.json())
+        .then(data =>{
+            raw.innerHTML += JSON.stringify(data["name"])
+        })
+    }   
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    llamadaApi()
+    cargarOpciones()
 });
